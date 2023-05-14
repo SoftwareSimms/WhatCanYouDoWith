67bb0f572b17424f7c25ef6a027a0a1f3f9e91c3
Jack Simms <129862066+SoftwareSimms@users.noreply.github.com>
2023-05-14 19:19:36 +0100

    2nd Sprint Commit
    added a functionality to search for terms, and provide a html popup that opens when clicked on the extension icon! Uses a json object to act as a database before attempting to incorporate DBpedia functionality.

---
+++ popup.js
@@ -0,0 +1,64 @@
+// Wait until the DOM content is loaded before executing the code
+document.addEventListener('DOMContentLoaded', function() {
+  // Get references to the popup.html UI elements
+  const searchInput = document.getElementById('search');
+  const searchButton = document.getElementById('searchButton');
+  const resultsDiv = document.getElementById('results');
+  
+  // Get the text from the search box
+  const urlParams = new URLSearchParams(window.location.search);
+  const searchQuery = urlParams.get('search');
+  
+  // If there's text in searchbox input, set it as the value of the search input and perform the search
+  if (searchQuery) {
+  searchInput.value = searchQuery;
+  search();
+  }
+  
+  // Add an event listener to the search button
+  searchButton.addEventListener('click', search);
+  
+  // Perform the search when the user clicks the search button
+  // **** NEED TO ADD PRESSING ENTER FUNCTIONALITY
+  async function search() {
+  console.log('Search button clicked');
+  try {
+    // Get the search text and convert it to lowercase
+    const searchText = searchInput.value.toLowerCase();
+    // Get the link to the database JSON file
+    const databaseLink = chrome.runtime.getURL('database.json');
+  
+    // Fetch the data from the database JSON file
+    const response = await fetch(databaseLink);
+    const nounData = await response.json();
+  
+    // Filter the data to find the noun matching the search text
+    const results = nounData.filter(function(db) {
+      return db.noun.toLowerCase() === searchText;
+    });
+  
+    // If results are found, display them in the results div
+    if (results.length > 0) {
+      while (resultsDiv.firstChild) {
+        resultsDiv.removeChild(resultsDiv.firstChild);
+      }
+      results[0].uses.forEach(function(use) {
+        const li = document.createElement('li');
+        li.textContent = use;
+        resultsDiv.appendChild(li);
+      });
+    } 
+    // If no results are found, display a message in the results div
+    else {
+      resultsDiv.textContent = 'Nothing found in the database.';
+    }
+  } 
+  // If an error occurs while fetching data or parsing JSON, display an error message in the results div
+  catch (error) {
+    console.error('Error fetching data:', error);
+    resultsDiv.textContent = 'An error occurred while searching the database. Please try again later.';
+  }
+  }
+  });
+
+
